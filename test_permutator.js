cf1ea344b61ba917a13eaed7ae1d9eea4c072cfb
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-05-21 15:07:56 +0100

    combinatorics | test_permutator update

--- test_permutator.js
+++ test_permutator.js
@@ -1,31 +1,35 @@
 const permutator = (inputArr) => {
+  const BLACK = '\033[0;30m', GREEN = '\033[1;32m', BLUE = '\033[0;34m';
   let result = [];
-  console.log('entering permutator', inputArr);
-  const permute = (arr, m = []) => {
-    console.log('---');
-    console.log('entering permute with arr', arr, 'm', m);
+  console.log('entering permutator, input array:', inputArr);
+  const permute = (arr, m = [], s='  ') => {
+    console.log(`${s}---`);
+    console.log(`${s}arr:`, arr, `m:`, m, `${GREEN}(entering permute)${BLACK}`);
     if (arr.length === 0) {
-      console.log();
-      console.log('===');
-      console.log('arr length == 0, at the bottom of recursion, saving m:', m);
+      console.log(`${s}===`);
+      console.log(`${s}${BLUE}arr length == 0, at the bottom of recursion, saving m:${BLACK}`, m);
       result.push(m)
-      console.log('result now', result);
-      console.log('===');
-      console.log();
+      console.log(`${s}${BLUE}result now:${BLACK}`, result);
+      console.log(`${s}===`);
     } else {
       for (let i = 0; i < arr.length; i++) {
         let curr = arr.slice();
         let next = curr.splice(i, 1);
-        console.log('arr/curr', arr, 'i', i, '-> next', next,  'm', m, '-> m+next', m.concat(next));
-        console.log('about to recurse with curr and m+next');
-        permute(curr.slice(), m.concat(next))
-        console.log('...back!');
+        console.log();
+        console.log(`${s}looping: ${i+1}/${arr.length}`);
+        console.log(`${s}current:`, curr, 'm:', m, 'next:', next,  '-> m+next:', m.concat(next), '(recursing with current and m+next)');
+        permute(curr.slice(), m.concat(next), `${s}  `)
+        if (arr.length > 1 && i < arr.length - 1) {
+          console.log(`${s}arr:`, arr, '(...back, looping!)');
+        } else {
+          console.log(`${s}arr:`, arr, '(...back and done!)');
+        }
       }
     }
   }
 
   permute(inputArr)
-  console.log();
+  console.log('----------------------------------------');
   console.log('exiting permutator, result', result);
 
   return result;
