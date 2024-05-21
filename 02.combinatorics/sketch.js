61e08cdc006dfb524d6de53419136248374b9164
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-05-21 15:07:37 +0100

    combinatorics 2 | update

--- 02.combinatorics/sketch.js
+++ 02.combinatorics/sketch.js
@@ -1,3 +1,23 @@
+// This sketch uses randomness to implement something like the 'beautiful
+// corpse' ('cadavre exquis'): the structure of the sentence is set in advance,
+// but it is composed of 'slots' that can be filled with interchangeable
+// elements.
+
+// Suggestions:
+// - The core idea here is that opposition between an overall structure (here
+// the sentence) and constituent parts / slots, that don't themselves move, but
+// can be filled with changing elements. This is a very abstract principle,
+// that could be applied in all sorts of ways: for instance, as soon as you
+// have a temporal sequence of events (perhaps during a performance, or a
+// number of animations one after the other), then you could use this principle
+// to inject randomness into which elements gets picked, even as you maintain
+// the overarching structure.
+// - It is perfectly possible to use randomness in this way to change other
+// elements, not just the text. A simple example of this can be to have a
+// reservoir of different fonts, or colours, etc., for the various slots?
+// In this case, you would have to split the string below into several parts,
+// and display each part individually.
+
 // define our variables
 let x,y,z,t,e,f;
 let s1;
@@ -5,12 +25,13 @@ let s2;
 
 function setup() {
 
-  createCanvas(500, 800);
+  createCanvas(400, 400);
 
   // our sentence
   s1 = "Introducing textual systems through experimental French poetry";
 
   // reservoirs/slots
+  // TODO: add more elements to these arrays
   y = ["Introducing"];
   z = ["textual"];
   t = ["systems"];
@@ -30,13 +51,14 @@ function setup() {
 
 function draw() {
 
-  background(255);
+  background(31, 240, 45);
 
   // ----------------------------------------
   // 2) Slots / Reservoirs
   // our sentences: the original and the modified one (see keyPressed)
-  text(s1, 10, 30, 300);
-  text(s2, 10, 200, 300);
+  const m = 40;
+  text(s1, m, m,        400 - m);
+  text(s2, m, height/2, 400 - m);
 
 }
 
@@ -56,5 +78,9 @@ function keyPressed() {
 // get one random element from an array
 
 function randEl(arr) {
+  //     ⌜ from this array
+  //     |   ⌜ (turn into integer)
+  //     |   |          pick a random index
+  //     ↓   ↓          ↓
   return arr[Math.floor(Math.random()*arr.length)];
 }
