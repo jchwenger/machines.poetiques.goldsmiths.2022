0a72f274cea9c81531562f197814a28a5b17777f
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-05-28 22:04:31 +0100

    combinatorics | 1, clean-up

--- 01.combinatorics.permutations/sketch.js
+++ 01.combinatorics.permutations/sketch.js
@@ -5,14 +5,6 @@
 // is very abstract, and could apply to all sorts of things. For instance, the
 // elements of the set could be different events in a narrative, performance,
 // or animation.
-// - Combinations are subsets, and the order of the elements does not matter.
-// This is equivalent to enumerating all the ways you can select a certain
-// number of elements from a set. K-combinations are only the selection of
-// subsets of a particular size k. This could be used if you want to combine
-// things together. For instance, if your set contains characters, combinations
-// could give you all possible ways of forming groups of characters. If your
-// set contains emotions, and you would like to create a piece combining three
-// together, you could look at all the k-combinations for k = 3.
 
 // define our variables
 let a, p;
@@ -27,12 +19,9 @@ function setup() {
   // (also, you may have to increase the canvas size)
   a = ["hello", "world", "goodbye", "universe"];
   // a = ["entrance", "roof", "basement", "staircase"];
+  // a = ["stand", "sit", "raise hand", "close eyes"];
 
-  // here three choices for permutations
-  // (use only one at a time!)
   p = permutator(a); // all permuations: shuffling! Order matters!
-  // p = combinator(a); //  all combinations (sets of various lengths). Order does not matter!
-  // p = k_combinator(a, 2); //  all k combinations (sets of length k). Order does not matter!
 
   // we prepare our canvas, text size, we don't cut words at the end of lines, and select a font
   fill(255);
