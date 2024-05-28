f7ce79bfeaddc8065ce8ddb60f3892ba0680c83e
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-05-28 22:59:08 +0100

    combinatorics | 3, update

--- 03.combinatorics.k_combinations/sketch.js
+++ 03.combinatorics.k_combinations/sketch.js
@@ -1,90 +1,69 @@
 // This sketch introduces the concepts of permutations and combinations
 
 // Suggestions:
-// - Permutations will yield all possible orderings of a set. Note that this
-// is very abstract, and could apply to all sorts of things. For instance, the
-// elements of the set could be different events in a narrative, performance,
-// or animation.
-// - Combinations are subsets, and the order of the elements does not matter.
-// This is equivalent to enumerating all the ways you can select a certain
-// number of elements from a set. K-combinations are only the selection of
-// subsets of a particular size k. This could be used if you want to combine
-// things together. For instance, if your set contains characters, combinations
-// could give you all possible ways of forming groups of characters. If your
-// set contains emotions, and you would like to create a piece combining three
-// together, you could look at all the k-combinations for k = 3.
+// - K-combinations are only the selection of subsets of a particular size k.
+// This could be used if you want to combine things together. For instance, if
+// your set contains characters, combinations could give you all possible ways
+// of forming groups of characters. If your set contains emotions, and you
+// would like to create a piece combining three together, you could look at all
+// the k-combinations for k = 3.
 
 // define our variables
-let a, p;
+let a, p, mySelect;
 
 function setup() {
 
-  createCanvas(500, 800);
+  createCanvas(800, 400);
 
-  // material for permutations and combinations
-  // BEWARE! The computation for this list grows very, very fast,
-  // and your browser will quickly run out of resources
+  // material for combinations
   // (also, you may have to increase the canvas size)
-  a = ["hello", "world", "goodbye", "universe"];
-  // a = ["entrance", "roof", "basement", "staircase"];
+  // a = ["happiness", "sadness", "excitement", "anger", "curiosity"];
+  a = ["entrance", "roof", "basement", "staircase", "kitchen"];
 
-  // here three choices for permutations
-  // (use only one at a time!)
-  p = permutator(a); // all permuations: shuffling! Order matters!
-  // p = combinator(a); //  all combinations (sets of various lengths). Order does not matter!
-  // p = k_combinator(a, 2); //  all k combinations (sets of length k). Order does not matter!
+  p = k_combinator(a, 2); //  all k combinations (sets of length k). Order does not matter!
 
   // we prepare our canvas, text size, we don't cut words at the end of lines, and select a font
-  fill(255);
+  fill(0);
   textSize(25);
   textWrap(WORD);
-  textAlign(CENTER);
+  textAlign(LEFT);
   textFont('Helvetica');
+
+  // reference: https://p5js.org/reference/#/p5/createSelect
+  mySelect = createSelect();
+  mySelect.position(805, 5);
+
+  // Add options programmatically
+  for (let i = 0; i < a.length; i++) {
+    mySelect.option(String(i+1)); // add as many numbers as there are 
+  }
+
+  // Set the selected option corresponding to the size of our k (2)
+  mySelect.selected(String(2));
 
 }
 
 function draw() {
 
-  background(245, 27, 241);
+  background(18, 252, 241);
 
   // display the results
   for (let i = 0; i < p.length; i++) {
-      text(p[i].join(', '), width/2, 50 + i*30);
+    text(p[i].join(' or '), 50, 50 + i*30);
   }
 
+  mySelect.changed(reCombine);
+
+}
+
+function reCombine() {
+  // when the user selects something, 
+  p = k_combinator(a, Number(mySelect.selected())); 
 }
 
 // ----------------------------------------
 // THE UNDERBELLY
 // ----------------------------------------
-
-// ----------------------------------------
-// Permutations
-// ------------
-
-// https://stackoverflow.com/a/20871714
-// generate all permutations of an array
-// Order matters! [a,b] =/= [b,a]
-
-const permutator = (inputArr) => {
-  let result = [];
-
-  const permute = (arr, m = []) => {
-    if (arr.length === 0) {
-      result.push(m)
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        let curr = arr.slice();
-        let next = curr.splice(i, 1);
-        permute(curr.slice(), m.concat(next))
-      }
-    }
-  }
-
-  permute(inputArr)
-
-  return result;
-}
 
 // ----------------------------------------
 // Combinations
