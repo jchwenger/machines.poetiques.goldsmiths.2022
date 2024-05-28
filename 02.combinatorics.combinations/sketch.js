ddd2e2780cc7863fe73380c19ed7f4ae0cc56f3a
jchwenger <34098722+jchwenger@users.noreply.github.com>
2024-05-28 22:49:14 +0100

    combinatorics | 2, fix

--- 02.combinatorics.combinations/sketch.js
+++ 02.combinatorics.combinations/sketch.js
@@ -53,34 +53,6 @@ function draw() {
 // ----------------------------------------
 
 // ----------------------------------------
-// Permutations
-// ------------
-
-// https://stackoverflow.com/a/20871714
-// generate all permutations of an array
-// Order matters! [a,b] =/= [b,a]
-
-const permutator = (inputArr) => {
-  let result = [];
-
-  const permute = (arr, m = []) => {
-    if (arr.length === 0) {
-      result.push(m)
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        let curr = arr.slice();
-        let next = curr.splice(i, 1);
-        permute(curr.slice(), m.concat(next))
-      }
-    }
-  }
-
-  permute(inputArr)
-
-  return result;
-}
-
-// ----------------------------------------
 // Combinations
 // ------------
 
